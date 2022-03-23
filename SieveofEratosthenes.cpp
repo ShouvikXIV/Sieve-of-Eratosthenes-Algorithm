2f58b074c6411258e9f419bb4d8638ad3f83f34b
Shouvik Barua <85792552+ShouvikXIV@users.noreply.github.com>
2022-03-23 15:38:52 +0600

    Update SieveofEratosthenes.cpp

--- SieveofEratosthenes.cpp
+++ SieveofEratosthenes.cpp
@@ -1,47 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void sieve(int r, int l)
+{
+    int arr1[l];
+    for(int i = 0;i<=l;i++)
+    {
+        if(i==0||i==1)
+        {
+            arr1[i]= 0;
+        }
+        else
+        {
+            arr1[i] = 1;
+        }
+    }
+
+    for(int i = 2;i*i<=l;i++)
+    {
+        if(arr1[i]==1)
+        {
+            for(int k = 2;i*k<=l;k++)
+            {
+                arr1[i*k] = 0;
+            }
+        }
+    }
+
+    for(int i = r;i<l;i++)
+    {
+        if(arr1[i]==1)
+        {
+            cout<<i<<' ';
+        }
+    }
+
+}
+
 int main()
 {
-	#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-	#endif
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n;
-    cin>>n;
-    std::vector<int> v(n);
-// 0 for non prime
-// 1 for prime
-// initially set all the value as 1 (prime) except 0 and 1
-    for(int i = 0;i<=n;i++)
-    {
-    	if(i==0||i==1)
-    	{
-    		v[i]=0;
-    	}
-    	else
-    	{
-    		v[i]=1;
-    	}
-    }
-    for(int i = 2;i<=n;i++)
-    {
-    	if(v[i]==1)
-    	{
-    		for(int k = 2;i*k<=n;k++)
-    		{	
-    			v[k*i] = 0;
-    		}
-    	}
-    }
-	for(int i = 0;i<=n;i++)
-	{
-		if(v[i]==1)
-		{
-			cout<<i<<' ';
-		}
-	}
-	return 0;
+
+    int r,l;
+    cin>>r>>l;
+    sieve(r,l);
+    return 0;
+
 }
